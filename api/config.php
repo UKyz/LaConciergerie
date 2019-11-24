423409e86d9d4884a7303b784270c5beb558d0e5
vfauquembergue <vfauquembergue@scalair.fr>
2019-11-24 21:39:15 +0100

    Fix vue bug in service and add config for production

--- api/config.php
+++ api/config.php
@@ -1,6 +1,7 @@
 <?php
 
 return function () {
+    // Victor configuration
     $host = "localhost";
     $user = "root";
     $password = "root";
@@ -9,7 +10,17 @@ return function () {
     $con = mysqli_connect($host, $user, $password, $dbname);
 
     if (!$con) {
+        // Alexia configuration
         $password = "";
+        $con = mysqli_connect($host, $user, $password,$dbname);
+    }
+
+    if (!$con) {
+        // 1and1 configuration
+        $host = "db5000231025.hosting-data.io";
+        $user = "dbu62777";
+        $dbname = "dbs225552";
+        $password = "L@conciergerie1";
         $con = mysqli_connect($host, $user, $password,$dbname);
     }
 
